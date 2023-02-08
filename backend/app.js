30a7ef16bcf4d1d2123f08aabfe6ac57ce540762
Deepak Kumar Verma <deepakramgarh2000@gmail.com>
2023-02-08 11:43:43 +0530

    Try on versal

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,20 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import path from "path";
+import cors from "cors";
+
 export const app = express();
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
 
 import { userRouter } from "./routes/UserRoute.js";
 app.use("/api/v1", userRouter);
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 import { app } from "./app.js";
-import dotenv from "dotenv";
-import { connectDatabase } from "./config/database.js";
 import cloudinary from "cloudinary";
+import RazorPay from "razorpay";
+import dotenv from "dotenv";
+import nodeCron from "node-cron";
+import { connectDatabase } from "./config/database.js";
+import { Stats } from "./models/Stats.js";
 
 // connection configuration to env file
 dotenv.config({ path: "./backend/config/config.env" });
@@ -10,9 +13,23 @@ connectDatabase();
 
 // connection cufiguration to cloudinary for images
 cloudinary.v2.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+});
+
+// crating instance of razorpay payment gateway
+export const instance = new RazorPay({
+  key_id: process.env.RAZORPAY_API_KEY,
+  key_secret: process.env.RAZORPAY_API_SECRET,
+});
+
+nodeCron.schedule("0 0 0 0 1 *", async () => {
+  try {
+    await Stats.create({});
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 app.listen(process.env.PORT, () => {
